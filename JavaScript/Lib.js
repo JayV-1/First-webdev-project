e5150e28badec021856dab005cd3d1e93bd68041
JayV-1 <fbarrosjoao@gmail.com>
2023-03-24 21:50:37 -0300

    Light mode begins
    The first phase of light mode.

--- JavaScript/Lib.js
+++ JavaScript/Lib.js
@@ -1,1 +1,10 @@
-const theme = document.querySelector(".bu")
+const themeButton = document.getElementById("btn")
+const dark = ["black","#E6B950", "#cc1f29"]
+const light = ["#D0BED4", ""]
+
+
+themeButton.addEventListener("click", function(){
+    Math.floor(Math.random)
+    document.body.style.backgroundColor = "#D0BED4";
+    console.log("dumb button")
+})
