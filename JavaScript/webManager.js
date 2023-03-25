a2c7112542c567bb9d5c866252d16a7ce9691d7d
JayV-1 <128651420+JayV-1@users.noreply.github.com>
2023-03-25 11:03:10 -0300

    Toggle theme simplified

--- JavaScript/webManager.js
+++ JavaScript/webManager.js
@@ -1,58 +1,6 @@
 const themeButton = document.getElementById("btn");
-const header = document.getElementsByTagName("ul")[0];
-
-let num = 0;
-
-function lightTheme(){
-    document.body.style.backgroundColor = "#675B6E";
-    document.getElementsByTagName("h1")[0].style.color = "white";
-    document.getElementsByTagName("h4")[0].style.color = "white";
-};
-
-function darkTheme(){
-    document.body.style.backgroundColor = "black";
-    document.getElementsByTagName("h1")[0].style.color = "#E6B950";
-    document.getElementsByTagName("h4")[0].style.color = "#E6B950";
-};
-
-function theming(){
-    if (num == 1) {
-        lightTheme()
-
-    } else if (num == 0) {
-        darkTheme()
-
-    };
-};
-
-function switchTheme(){
-    if (num == 1) {
-        num = 0
-
-    } else if (num == 0) {
-        num = 1
-
-    }; 
-};
-
-header.addEventListener("click", function(){
-    console.log(header)
-    try{
-        
-        // wait(10000);
-        theming();  
-    } catch(err){
-        console.log(err)
-    }
-    
-});
+const bodyStyle = document.body
 
 themeButton.addEventListener("click", function(){
-    console.log(num)
-    
-    
-    switchTheme()
-    theming()
-    console.log("dumb button")
+        bodyStyle.classList.toggle("lightMode"); 
 });
-
